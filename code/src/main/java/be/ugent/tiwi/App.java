07876a2dc186173febbc7e2e8883e7825212bede
Jelle De Bock <debockjelle@hotmail.com>
2016-02-16 12:15:43 +0100

    Config loading done

--- code/src/main/java/be/ugent/tiwi/App.java
+++ code/src/main/java/be/ugent/tiwi/App.java
@@ -1,5 +1,7 @@
 package be.ugent.tiwi;
 
+import settings.Settings;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Settings.createSettings();
+        System.out.println("Settings created!");
     }
 }

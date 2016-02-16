1d6a00d0447be06f9e263daf6a3b888753089c62
Jelle De Bock <debockjelle@hotmail.com>
2016-02-16 10:02:30 +0100

    Settings rewrite

---
+++ verkeer3-code/src/main/java/Verkeer3.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Verkeer3 {
+    public static void main(String []args)
+    {
+        System.out.println("Hello world");
+        Scanner s = new Scanner(System.in);
+        s.next();
+    }
+}

fbdc72f707abc70b9caf8747fc2c6b023cc4c197
jjverven <jan.vervenne@ugent.be>
2016-03-05 19:08:02 +0100

    waze scraper

--- Perl/WazeScraper.pl
+++ Perl/WazeScraper.pl
@@ -2,13 +2,11 @@ use URI;
 use Web::Scraper;
 use Encode;
 
-# First, create your scraper block
+# Scraper blok aanmaken
 my $route = scraper {
-	# Parse all TDs inside 'table[width="100%]"', store them into
-	# an array 'routes'.  We embed other scrapers for each TD.
-	process '.waze-header .waze-header-menu', "routes[]" => scraper {
-		# And, in each TD,
-		# get the URI of "a" element
+	# In welk blok gekeken moet worden
+	process "body", "routes[]" => scraper {
+		# Haal info op van elementen uit het blok
 		process "a", name => 'TEXT';
 	};
 };
@@ -21,13 +19,9 @@ my $uri = "https://www.waze.com/livemap?ll=13&lat=51.05175&lon=3.72711&navigate=
 				"&to_lon=3.70653" .
 				"&at_req=0&at_text=Now";
 
-my $res = $route->scrape( URI->new("https://www.waze.com/livemap") );
+my $res = $route->scrape( URI->new($uri) );
 
-# iterate the array 'authors'
+# Overloop de routes
 for my $route (@{$res->{routes}}) {
-	# output is like:
-	# Andy Adler      http://search.cpan.org/~aadler/
-	# Aaron K Dancygier       http://search.cpan.org/~aakd/
-	# Aamer Akhter    http://search.cpan.org/~aakhter/
 	print Encode::encode("utf8", "$route->{name}\n");
 }

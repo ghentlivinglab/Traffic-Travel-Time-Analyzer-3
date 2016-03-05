6c048c1f7d633e2a1f57c8059070ecf27ecfc3d1
jgstoffe <Jeroen.Stofferis@UGent.be>
2016-03-05 19:41:53 +0100

    ui
    rcyvbn

---
+++ code/src/main/java/be/ugent/tiwi/domein/tomtom/Leg.java
@@ -0,0 +1,81 @@
+
+package be.ugent.tiwi.domein.tomtom;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Generated("org.jsonschema2pojo")
+@JsonPropertyOrder({
+    "summary",
+    "points"
+})
+public class Leg {
+
+    @JsonProperty("summary")
+    private Summary_ summary;
+    @JsonProperty("points")
+    private List<Point> points = new ArrayList<Point>();
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    /**
+     * 
+     * @return
+     *     The summary
+     */
+    @JsonProperty("summary")
+    public Summary_ getSummary() {
+        return summary;
+    }
+
+    /**
+     * 
+     * @param summary
+     *     The summary
+     */
+    @JsonProperty("summary")
+    public void setSummary(Summary_ summary) {
+        this.summary = summary;
+    }
+
+    /**
+     * 
+     * @return
+     *     The points
+     */
+    @JsonProperty("points")
+    public List<Point> getPoints() {
+        return points;
+    }
+
+    /**
+     * 
+     * @param points
+     *     The points
+     */
+    @JsonProperty("points")
+    public void setPoints(List<Point> points) {
+        this.points = points;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}

bb4715972d6c0154281d3e28107e236e2f14f1bb
brflor <brentflore@gmail.com>
2016-05-06 18:19:13 +0200

    Revert "probleem incidentenlijst oplossen+deploy"
    This reverts commit e9cc7c826695489f1638a5186bb24269f881890f.

--- code/src/main/java/be/ugent/tiwi/App.java
+++ code/src/main/java/be/ugent/tiwi/App.java
@@ -1,7 +1,6 @@
 package be.ugent.tiwi;
 
 import be.ugent.tiwi.controller.ScheduleController;
-import be.ugent.tiwi.dal.IncidentRepository;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -10,13 +9,9 @@ import java.io.IOException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.sql.SQLException;
-import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        IncidentRepository rep = new IncidentRepository();
-        List result = rep.getTrafficIncidentsTrajecten();
-        result.size();
         ScheduleController scheduleC = new ScheduleController();
         try {
             scheduleC.start();

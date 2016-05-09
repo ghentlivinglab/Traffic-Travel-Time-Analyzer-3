e1abce37167df281243595633a391db0abefae6d
brflor <brentflore@gmail.com>
2016-05-09 10:37:02 +0200

    aanvullen deployments

---
+++ deployment/eerste deployment.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+command -v svn >/dev/null 2>&1 || { echo "Dit script maakt gebruik van svn (svn (subversion). Gelieve dit te installeren voor gebruik" >&2; exit 1; }; svn export https://github.ugent.be/iii-vop2016/verkeer-3/trunk/deployment . --force --username $1 --password $2;  asadmin deploy --name verkeersCentrum3 verkeer3gui.war;

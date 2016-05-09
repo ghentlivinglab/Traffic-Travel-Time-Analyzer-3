dbf4f92882d92a1a1b08d5436fb150525fd30d13
brflor <brentflore@gmail.com>
2016-05-09 10:00:11 +0200

    aanpassing script

--- deployment/deployment.sh
+++ deployment/deployment.sh
@@ -1,1 +1,4 @@
-command -v svn >/dev/null 2>&1 || { echo "Dit script maakt gebruik van svn (subversion). Gelieve dit te installeren voor gebruik" >&2; svn export https://github.ugent.be/iii-vop2016/verkeer-3/trunk/deployment . --force --username $1 --password $2; asadmin redeploy --name verkeersCentrum3 verkeer3gui.war; exit 1; }
+#!/bin/bash
+command -v svn >/dev/null 2>&1 || { echo "Dit script maakt gebruik van svn (svn (subversion). Gelieve dit te installeren voor gebruik" >&2; exit 1; }  svn export https://github.ugent.be/iii-vop2016/verkeer-3/trunk/deployment . --force --username $1 --password $2 >&2; printf "test\n" >&1; asadmin redeploy --name verkeersCentrum3 verkeer3gui.war;
+
+
